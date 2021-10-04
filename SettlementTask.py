9b93512333076bd32b882cf56ceff969d5677648
Igorexy <iak7@bk.ru>
2021-10-04 18:01:34 +0300

    point 1 complete

--- SettlementTask.py
+++ SettlementTask.py
@@ -1,17 +1,26 @@
 import statistics as stat
+import pandas as pd
 x = [[0]*8, [0]*8, [0]*8]
 file = open("Data.txt")
 j = 0; k = 0; buf = ''
 #Считывание из файла
 for line in file:
-    for i in range(len(line)):
-        if line[i] == ' ' or line[i] == '\n':
-            x[j][k] = buf; j += 1
-            buf = ''
-            continue
-        else:
-            buf += line[i]
-    j = 0; k += 1
+    values  = list(map(float, line.split()))
+    x[0][k] = values[0]
+    x[1][k] = values[1]
+    x[2][k] = values[2]
+    k += 1
 #Считывание из файла завершено
-m = stat.mean(x[1])
-print(m)
+DF = pd.DataFrame(x).transpose()
+M = DF.mean()   # Мат ожидание
+SD = DF.std()   # Дисперсия
+covMatrix = DF.cov()    # Ковариационная матрица
+corrMatrix = DF.corr()  # Корреляционная матрица
+
+
+# Вывод данных
+print("Исходные данные\n", DF)
+print("Математические ожидания\n", M)
+print("Дисперсии\n", SD)
+print("Ковариационная матрица\n", covMatrix)
+print("Корреляционная матрица\n", corrMatrix)
